d84502fa0cc001007218637f96b04616a51cb271
rakibul-in <rakibulhasan.nlb@gmail.com>
2023-07-24 14:07:21 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ replit.nix
@@ -0,0 +1,14 @@
+{ pkgs }: {
+  deps = [
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.\
+    pkgs.nodePackages.vscode-langservers-extracted
+    pkgs.nodePackages.typescript-language-server  
+  ];
+}
